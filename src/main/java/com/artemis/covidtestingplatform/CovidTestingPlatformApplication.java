ab0ef2386973aef882d08bbf8d4f0df23db9eac5
Jeena <jeenathampi.23@gmail.com>
2020-09-07 22:39:16 -0700

    Frst push

---
+++ src/main/java/com/artemis/covidtestingplatform/CovidTestingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.artemis.covidtestingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTestingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTestingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemis/covidtestingplatform/CovidTestingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemis.covidtestingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTestingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
