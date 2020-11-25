3e3408efc4eba40d1ae50a83671f8827e8526bba
Jeena <jeenathampi.23@gmail.com>
2020-11-25 12:08:07 -0800

    commented tests

--- src/test/java/com/artemis/covidtestingplatform/CovidTestingPlatformApplicationTests.java
+++ src/test/java/com/artemis/covidtestingplatform/CovidTestingPlatformApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CovidTestingPlatformApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
