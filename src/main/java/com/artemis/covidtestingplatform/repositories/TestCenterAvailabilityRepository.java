7313cb8c855875285e8896187df64901511222e3
Jeena <jeenathampi.23@gmail.com>
2020-11-29 13:10:01 -0800

    get availability on test center id

--- src/main/java/com/artemis/covidtestingplatform/repositories/TestCenterAvailabilityRepository.java
+++ src/main/java/com/artemis/covidtestingplatform/repositories/TestCenterAvailabilityRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestCenterAvailabilityRepository extends CrudRepository<TestCenterAvailability,String> {
+   TestCenterAvailability findByTestCenter_TestCentreId(String id);
 }
